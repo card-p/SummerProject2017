a043664fbc38ee5a80498cbcc4abab5ef3cdf512
SenaSakata <b1013217@fun.ac.jp>
2017-08-23 18:18:59 +0900

    main create

--- MainFrame.java
+++ MainFrame.java
@@ -3,30 +3,24 @@ import javax.swing.*;
 //import javax.swing.border.*;
 import java.awt.*;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JPanel{
+	Main mainSys;
 
-	public static void main(String[] args){
-		MainFrame frame= new MainFrame("ãƒ‘ã‚¹ãƒ¯ãƒ¼ãƒ‰ç®¡ç†ãƒ„ãƒ¼ãƒ«");
-		frame.setVisible(true);
-	}
-
-	MainFrame(String title){
-		setTitle(title);
-		setBounds(100, 100, 640, 480);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel panel1 = new JPanel();
-		panel1.setBackground(Color.WHITE);
-
-		// ãƒ†ã‚­ã‚¹ãƒˆãƒ•ã‚£ãƒ¼ãƒ«ãƒ‰ä½œæˆ
-		JTextField text1 = new JTextField("ãƒ¦ãƒ¼ã‚¶åã‚’å…¥åŠ›");
+	MainFrame(Main m){
+		mainSys = m;
+		// ƒ†[ƒU–¼“ü—ÍƒtƒH[ƒ€
+		JTextField text1 = new JTextField("ƒ†[ƒU–¼‚ğ“ü—Í");
 		text1.setColumns(15);
 		text1.setBounds(100,100,100,100);
 
-		panel1.add(text1);
+		this.add(text1);
 
-		Container contentPane = getContentPane();
-		contentPane.add(panel1, BorderLayout.CENTER);
+		// Container contentPane = getContentPane();
+		// contentPane.add(panel1, BorderLayout.CENTER);
 // tintin
 	}
+
+	public void pc(String str){
+        mainSys.PanelChange((JPanel)this, str);
+    }
 }
